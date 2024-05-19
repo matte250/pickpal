8ee78bb3873122343f81c1cbf1c871dd93086247
Mattias Soroccos <mattias.soroccos@gmail.com>
2024-05-19 19:16:48 +0200

    fix app schema

--- supabase/migrations/20240519155650_add-base-db-types.sql
+++ supabase/migrations/20240519155650_add-base-db-types.sql
@@ -1,13 +1,19 @@
-create table if not exists public.sessions (
+create schema if not exists app;
+
+grant usage on schema app to service_role;
+grant all privileges on all tables in schema app to service_role;
+
+create table if not exists app.sessions (
   id uuid primary key default gen_random_uuid(),
   user_id uuid references auth.users(id),
   location geography(POINT) not null,
   created_at timestamp with time zone default now()
 );
 
-create table if not exists public.places (
+create table if not exists app.places (
   id serial primary key,
   name text not null,
   location geography(POINT) not null,
   image_urls text[] default '{}' not null
 );
+
